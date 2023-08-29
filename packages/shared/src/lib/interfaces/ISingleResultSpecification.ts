91c9d3923bbb143b580f3fc385a2b4380c010f7f
Ian Munnerley <ian@epicware.dev>
2023-08-29 14:39:39 -0400

    build fixes

--- packages/shared/src/lib/interfaces/ISingleResultSpecification.ts
+++ packages/shared/src/lib/interfaces/ISingleResultSpecification.ts
@@ -1,4 +1,4 @@
 import { ISpecification } from './ISpecification';
 
 // eslint-disable @typescript-eslint/no-empty-interface
-export type ISingleResultSpecification<T> = ISpecification<T>;
+export type ISingleResultSpecification<T> = ISpecification<T, any>;
